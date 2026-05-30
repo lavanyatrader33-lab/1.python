d1b3c1301cfdf6c72fd1613af88cbb0342770bfa
lavanyatrader33-lab <lavanya.trader33@gmail.com>
2026-05-30 18:51:48 +0530

    Create BMI_check.py

---
+++ BMI_check.py
@@ -0,0 +1,13 @@
+BMI=int(input("Enter the BMI"))
+if(BMI<18.5):
+    print("Underweight")
+elif(BMI<24.9):
+    print("Normal")
+elif(BMI<29.9):
+    print("Overweight")
+elif(BMI<34.9):
+    print("Obese")
+elif(BMI<39.9):
+    print("Very obese")
+else:
+    print("Severly obse")
